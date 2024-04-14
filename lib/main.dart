d7554a14fab8bbd6e6e8d605272b0ee7c1a00240
Carlosmon04 <dcarlosmontoya04@gmail.com>
2024-04-13 21:24:56 -0600

    sisisi

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
+import 'package:flutterfirebase/firebase_options.dart';
+import 'package:flutterfirebase/new_user_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-
-await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
 
-FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-NotificationSettings setting =  await messaging.requestPermission(
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
 
-alert: true,
-sound: true,
-badge: true,
-announcement:  false,
-carPlay: false,
-provisional: false
-);
+  print('User granted permission: ${settings.authorizationStatus}');
 
-print('user granted permision: ${setting.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
 
-FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-  print('Got a message whilst in the foreground!');
-  print('message data : ${message.data}');
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
 
-  if(message.notification !=null){
-    print('message also contained a notification : ${message.notification}');
-  }
- });
-
-runApp(const MyApp());
-
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-   FirebaseMessaging messaging = FirebaseMessaging.instance;
-   messaging.getToken().then(   (token){
-    print('Token: $token');
-   } );
+    messaging.getToken().then((token) {
+      //TODO: guardar en firebase
+      print('Token: $token');
+    });
 
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Usuarios'),
+      routes: {
+        '/new_user': (context) => NewUserPage(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
@@ -75,44 +75,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-   
-      _counter++;
-    });
-  }
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
-  
+    // final users = firestore.collection('usuarios').get();
+    final users = firestore.collection('usuarios').snapshots();
+
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: users,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final listaUsuarios = snapshot.data!.docs; // La lista de documentos
+
+            return ListView.builder(
+              itemCount: listaUsuarios.length,
+              itemBuilder: (context, index) {
+                final user = listaUsuarios[index];
+
+                // user.id;
+
+                return ListTile(
+                  title: Text(user['nombre']),
+                  subtitle: Text(user['correo']),
+                  trailing: Text('${user['telefono']}'),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          Navigator.pushNamed(context, '/new_user');
+        },
+        tooltip: 'Nuevo usuario',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
